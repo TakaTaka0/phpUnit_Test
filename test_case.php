0ccf0ede0a318479830da0dc9e3eddaaa7d61960
TakaTaka 0 <tk.1987.6.9@gmail.com>
2018-01-28 17:58:49 +0000

    test phpunit

---
+++ test_case.php
@@ -0,0 +1,12 @@
+<?php 
+//test PHPUnit
+
+class MyTestCase extends PHPUnit_Framework_TestCase {
+    public function test_test1() {
+        echo "test1\n";
+    }
+    public function test_test2() {
+        echo "test2\n";            
+    }
+}
+?>
